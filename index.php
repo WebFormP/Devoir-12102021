2d3f1773196af4089e4ce01b1e836306346a0dcd
Pierre <pierre.sandron@cf2m.be>
2021-10-20 10:54:11 +0200

    Ajout des pages PHP
    transformation des pages HTML en PHP

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include('menu.php'); ?>
+            <main>
+                <h1>Bienvenue sur notre site consacré à Bruxelles.</h1>
+                <p class="entete"><img src="images/Armoiries.png" /></p>
+                <h3>Bruxelles (Brussel en néerlandais, Brüssel en allemand, Brussels en anglais) est la capitale de la Belgique et l'un des sièges de l'Union Européenne et de plusieurs de ses institutions. Elle est le siège des gouvernements et parlements de plusieurs des entités fédérales qui composent la Belgique, ainsi que de nombreuses organisations internationales, dont l'OTAN.</h3>
+                <p class="entete">Dans ce site, vous trouverez diverses informations concernant :</p>
+                <ul class="sommaire">
+                    <li><a href="geographie.php">Une brève présentation de Bruxelles sur son rôle en Belgique et dans le monde</a></li>
+                    <li><a href="histoire.php">Un rappel historique, de sa fondation à aujourd'hui</a></li>
+                    <li><a href="culture.php">Un tour d'horizon en matière d'art et culture</a></li>
+                    <li><a href="galerie.php">Une galerie de photographies</a></li>
+                    <li><a href="contact.php">Un formulaire pour nous contacter</a></li>
+                    <li><a href="liens.php">Des liens vers des sites partenaires</a></li>
+                </ul>
+                <p class="entete"><a target="_blank" href="https://fr.wikipedia.org/wiki/Ville_de_Bruxelles">Sources : Wikipedia - Ville de Bruxelles</a></p>
+            </main>
+        </div>
+    </body>
+</html>
